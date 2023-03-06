70d55eda498a9cc8a4fe86df28080f1e3faa1796
NdaruVictor <ndaruvictor55@gmail.com>
2023-03-06 15:21:39 +0700

    first commit

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,6 @@
+    <video class="w-16 rounded-full " autoplay loop muted>
+        {{ $attributes }}
+        <source
+          src="{{ asset('video/micky.mp4')}}"
+          type="video/mp4" />
+      </video>
